f51271de39413260c75c2c37f8eb10b4d9e9985a
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-09-01 14:09:38 +1000

    Update Shit.php

--- src/skyss0fly/ShitBlock/Shit.php
+++ src/skyss0fly/ShitBlock/Shit.php
@@ -10,25 +10,24 @@ use pocketmine\player\Player;
 use pocketmine\world\Position;
 class Shit extends PluginBase {
  
-public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
-  switch ($command->getName()) {
-            case "shit":
-   if (!$sender->hasPermission("ShitBlock.TakaAFatShit")){
-    $sender->sendMessage(TEXTFORMAT::RED . "Bruh no perms");
-    return false;
-   }
-   else {
-      $x = $sender->getPosition()->getX();
-        $y = $sender->getPosition()->getY();
-        $z = $sender->getPosition()->getZ();
-    
 
- $block = $this->getServer()->getWorldManager()->getWorld()->getBlockAt($x . $y -1 . $z);
-     $this->getServer()->getWorldManager()->getWorld()->setBlock($block, VanillaBlocks::MUD_BLOCK());
-    $this->getServer()->broadcastMessage(TEXTFORMAT::BOLD . TEXTFORMAT::YELLOW . $sender->getName() . " Took a Fat Shit at " . $x . " " . $y . " " . $z . " !");
-    return true;
-}
-}
- return false;
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+    switch ($command->getName()) {
+        case "shit":
+            if (!$sender->hasPermission("ShitBlock.TakaAFatShit")){
+                $sender->sendMessage(TEXTFORMAT::RED . "Bruh no perms");
+                return false;
+            } else {
+                $x = $sender->getPosition()->getX();
+                $y = $sender->getPosition()->getY();
+                $z = $sender->getPosition()->getZ();
+                
+                $block = $this->getServer()->getWorldManager()->getWorld()->getBlockAt($x, $y - 1, $z);
+                $this->getServer()->getWorldManager()->getWorld()->setBlock($block, VanillaBlocks::MUD _BLOCK());
+                $this->getServer()->broadcastMessage(TEXTFORMAT::BOLD . TEXTFORMAT::YELLOW . $sender->getName() . " Took a Fat  sh1t at " . $x . " " . $y . " " . $z . " !");
+                return true;
+            }
+    }
+    return false;
 }
 }
