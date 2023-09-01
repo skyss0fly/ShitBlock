0731d7eed5e66335ce1cfb428bff4db158884d02
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-09-01 13:56:20 +1000

    Update Shit.php

--- src/skyss0fly/ShitBlock/Shit.php
+++ src/skyss0fly/ShitBlock/Shit.php
@@ -5,18 +5,15 @@ namespace skyss0fly\ShitBlock;
 use pocketmine\plugin\PluginBase;
 use pocketmine\{command\Command ,utils\TextFormat, world\WorldManager, block\VanillaBlocks, command\CommandSender , player\Player};
 
-class main extends PluginBase {
+class Shit extends PluginBase {
  
 public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
   switch ($command->getName()) {
             case "shit":
    $player = $sender->getPlayer();
    if ($sender->hasPermission("ShitBlock.TakaAFatShit")){
-$x = $player->getPosition()->getX();
-$y = $player->getPosition()->getY();
-$z = $player->getPosition()->getZ();
     $world = $this->getServer()->getWorldManager()->getWorld();
- $block = $world->getBlockAt($player->getPosition()->getFloorX(),$player->getPosition()->getFloorY() - 1, $player->getPosition()->getFloorZ());
+ $block = $world->getBlockAt($sender->getPosition()->getFloorX(),$sender->getPosition()->getFloorY() - 1, $sender->getPosition()->getFloorZ());
     $world->setBlock($block, VanillaBlocks::MUD_BLOCK());
     $this->getServer()->broadcastMessage(TEXTFORMAT::BOLD . TEXTFORMAT::YELLOW . $sender->getName() . " Took a Fat Shit at " . $x . " " . $y . " " . $z . " !");
     return true;
