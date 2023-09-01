c5a6f34154c0af8c46a79e9fde54070cdf6dbf21
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-09-01 14:19:17 +1000

    Update Shit.php

--- src/skyss0fly/ShitBlock/Shit.php
+++ src/skyss0fly/ShitBlock/Shit.php
@@ -1,40 +1,44 @@
+
 <?php
+
 namespace skyss0fly\ShitBlock;
 
-
 use pocketmine\plugin\PluginBase;
-use pocketmine\{ utils\TextFormat, world\WorldManager, block\VanillaBlocks};
+use pocketmine\utils\TextFormat;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\world\Position;
-class Shit extends PluginBase {
- 
+use pocketmine\block\VanillaBlocks;
 
-public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
-         $x = $sender->getPosition()->getX();
-        $y = $sender->getPosition()->getY();
-        $z = $sender->getPosition()->getZ();
- if (!$sender instanceof Player) {
-            $this->getLogger()->warning("Please use this command in-game");
+class Shit extends PluginBase {
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage(TextFormat::RED . "Please use this command in-game");
             return false;
         }
- if ($sender instanceof Player){
-    switch ($command->getName()) {
-        case "shit":
-            if (!$sender->hasPermission("ShitBlock.TakaAFatShit")){
-                $sender->sendMessage(TEXTFORMAT::RED . "Bruh no perms");
-                return false;
-            } else {
-             
-                
-                $block = $this->getServer()->getWorldManager()->getWorld()->getBlockAt($x, $y - 1, $z);
-                $this->getServer()->getWorldManager()->getWorld()->setBlock($block, VanillaBlocks::MUD_BLOCK());
-                $this->getServer()->broadcastMessage(TEXTFORMAT::BOLD . TEXTFORMAT::YELLOW . $sender->getName() . " Took a Fat  sh1t at " . $x . " " . $y . " " . $z . " !");
+
+        if ($command->getName() === "shit") {
+            if (!$sender->hasPermission("ShitBlock.TakaAFatShit")) {
+                $sender->sendMessage(TextFormat::RED . "Bruh no perms");
+                return false; 
+
+} else {
+                $x = $sender->getPosition()->getX();
+                $y = $sender->getPosition()->getY();
+                $z = $sender->getPosition()->getZ();
+
+                $worldManager = $this->getServer()->getWorldManager();
+                $world = $worldManager->getWorld($sender->getWorld());
+                $block = $world->getBlockAt($x, $y - 1, $z);
+                $block->setVanillaBlock(VanillaBlocks::MUD_BLOCK());
+                $world->setBlock($block);
+
+                $this->getServer()->broadcastMessage(TextFormat::BOLD . TextFormat::YELLOW . $sender->getName() . " Took a Fat sh1t at " . $x . " " . $y . " " . $z . " !");
                 return true;
             }
+        }
+        return false;
     }
-    return false;
-}
-}
 }
