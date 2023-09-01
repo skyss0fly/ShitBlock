931eaf0686e304d07185f076214e165136d0499d
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-09-01 13:34:32 +1000

    Update Shit.php

--- src/akyss0fly/ShitBlock/Shit.php
+++ src/akyss0fly/ShitBlock/Shit.php
@@ -6,8 +6,14 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\{command\Command , command\CommandSender , player\Player};
 
 class main extends PluginBase {
+ 
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+  switch ($command->getName()) {
+            case "shit":
 $x = $sender->getPosition()->getX();
 $y = $sender->getPosition()->getY();
 $z = $sender->getPosition()->getZ();
  $block = $world->getBlockAt($sender->getPosition()->getFloorX(),$sender->getPosition()->getFloorY() - 1, $sender->getPosition()->getFloorZ())
+}
+}
 }
